29f21ac1883ca824d082118ea7726ba4e2c38bdf
Minh Quang <quangminhnguyen3002@gmail.com>
2022-04-18 16:41:29 +0700

    add

--- src/app/component/home/store-list/store-list.component.ts
+++ src/app/component/home/store-list/store-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HomeService} from "../../../service/home.service";
 import {Store} from "../../../model/store";
 import {StoreCategories} from "../../../model/store-categories";
+import {CustomerService} from "../../../service/customer.service";
+import {TokenStorageService} from "../../../service/auth/token-storage.service";
 
 @Component({
   selector: 'app-store-list',
@@ -13,50 +15,94 @@ export class StoreListComponent implements OnInit {
   stores!: Store[];
   categories!: StoreCategories[];
   message: string = '';
+  isLoggedIn = false;
+  account_id?: number;
+  flag: boolean[] = [];
 
-  constructor(private customerService: HomeService) { }
+  constructor(private homeService: HomeService,
+              private customerService: CustomerService,
+              private tokenStorageService: TokenStorageService) {
+  }
 
   ngOnInit() {
     this.getAllStore();
     this.getAllCategories();
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      // @ts-ignore
+      this.account_id = this.tokenStorageService.getUser().id;
+    }
   }
 
   getAllStore() {
-    this.customerService.getAllStore().subscribe(data => {
+    this.homeService.getAllStore().subscribe(data => {
       this.stores = data;
+      this.flag = [];
+      for (let a of this.stores) {
+        this.customerService.checkLikeStore(a.id, this.account_id).subscribe((data) => {
+          this.flag.push(data)
+          console.log(data)
+
+        })
+      }console.log(this.flag)
     })
   }
 
   findAllByCategoriesList_Id(id: any) {
-    this.customerService.findAllByCategoriesList_Id(id).subscribe(data => {
+    this.homeService.findAllByCategoriesList_Id(id).subscribe(data => {
       this.stores = data;
     })
   }
 
   getAllCategories() {
-    this.customerService.getAllCategories().subscribe(data => {
+    this.homeService.getAllCategories().subscribe(data => {
       this.categories = data;
     })
   }
 
   topStoreSale() {
-    this.customerService.topStoreSale().subscribe(stores => {
+    this.homeService.topStoreSale().subscribe(stores => {
       this.stores = stores;
     });
   }
 
 
   searchStore() {
-      if (this.message == "") {
-        this.getAllStore();
-      } else {
-        this.customerService.searchStoreByNameContaining(this.message).subscribe(data => {
-          this.stores = data;
-        })
+    if (this.message == "") {
+      this.getAllStore();
+    } else {
+      this.homeService.searchStoreByNameContaining(this.message).subscribe(data => {
+        this.stores = data;
+      })
     }
   }
 
   saveId(id: any, name: string) {
     localStorage.setItem(name, id);
+  }
+
+  likeStore(store_id: any) {
+    this.customerService.likeStore(store_id, this.account_id).subscribe(
+      () => {
+        this.getAllStore();
+      })
+  }
+
+  listStoreLike(account_id: any) {
+    this.customerService.listStoreLike(account_id).subscribe(this.getAllStore)
+  }
+
+  getAllStoreFavorite() {
+    this.customerService.listStoreLike(this.account_id).subscribe((data) => {
+      this.stores = data;
+      this.flag = [];
+      for (let a of this.stores) {
+        this.customerService.checkLikeStore(a.id, this.account_id).subscribe((data) => {
+          this.flag.push(data)
+
+        })
+      }console.log(this.flag)
+    })
   }
 }
